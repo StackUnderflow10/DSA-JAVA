30eb5049163954f643a5cb226625b3c69caf6621
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-07-17 20:44:02 +0530

    SortedRecursion of array

---
+++ src/SortedRecursion.java
@@ -0,0 +1,12 @@
+public class SortedRecursion {
+    public static void main(String[] args) {
+        int[] arr ={1,10,7,9,10};
+        System.out.println(sorted(arr,0));
+    }
+    static boolean sorted(int[] arr, int index){
+        if(index == arr.length - 1){
+            return true;
+        }
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+}
